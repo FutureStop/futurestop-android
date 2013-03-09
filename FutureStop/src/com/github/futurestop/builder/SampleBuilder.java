57ac063b49df6514e62b5c223080a51c9cc2263f
Chung-Yi Cho <chung-yi.cho@mutualmobile.com>
2013-03-09 13:37:27 -0600

    Added gson supports.

--- FutureStop/src/com/github/futurestop/builder/SampleBuilder.java
+++ FutureStop/src/com/github/futurestop/builder/SampleBuilder.java
@@ -5,6 +5,7 @@ package com.github.futurestop.builder;
 
 import com.github.futurestop.model.FSResult;
 import com.github.futurestop.request.FSRequest;
+import com.google.gson.Gson;
 
 /**
  * @author Chung-Yi Cho
@@ -15,8 +16,12 @@ public class SampleBuilder implements IBuilder {
     @SuppressWarnings("unchecked")
     @Override
     public <T> FSResult<T> execute(FSRequest reqeust) {
+        Gson gson = new Gson();
+        String str = gson.fromJson(gson.toJson("Hello Sample Builder"), String.class);
+        
         FSResult<String> result = new FSResult<String>();
-        result.data = "Hello Chung-Yi Builder";
+        result.data = str;
+        
         return (FSResult<T>) result;
     }
 
