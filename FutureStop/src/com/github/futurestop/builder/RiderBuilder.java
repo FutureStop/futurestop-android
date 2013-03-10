dfdb7f8e80386376ee92cfa943a7d2c60a1e20c6
Mister X <MMorg76114@gmail.com>
2013-03-09 21:28:22 -0600

    Include ETA in json body.

--- FutureStop/src/com/github/futurestop/builder/RiderBuilder.java
+++ FutureStop/src/com/github/futurestop/builder/RiderBuilder.java
@@ -4,11 +4,14 @@
 package com.github.futurestop.builder;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
 
 import com.github.futurestop.model.FSResult;
 import com.github.futurestop.request.FSRequest;
@@ -29,11 +32,14 @@ public class RiderBuilder implements IBuilder {
     @Override
 	public <T> FSResult<T> execute(FSRequest reqeust) {
 		String url = URL + mId;
-
-		DefaultHttpClient client = new DefaultHttpClient();
-		HttpPost postRequest = new HttpPost();
-		postRequest.setURI(URI.create(url.trim()));
+		String ETA = generateETA();
 		try {
+			DefaultHttpClient client = new DefaultHttpClient();
+			HttpPost postRequest = new HttpPost();
+			postRequest.setURI(URI.create(url.trim()));
+			StringEntity dataSentToServer;
+			dataSentToServer = new StringEntity(ETA.trim(), HTTP.UTF_8);
+			postRequest.setEntity(dataSentToServer);
 			client.execute(postRequest);
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
@@ -42,7 +48,11 @@ public class RiderBuilder implements IBuilder {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 		return null;
 	}
+    
+    private String generateETA() {
+    	//TODO: Use Gson. Randomize ETA. Maybe base it off a user selected location.
+    	return "{ \"eta\" : \" 5000 \" }";
+    }
 }
